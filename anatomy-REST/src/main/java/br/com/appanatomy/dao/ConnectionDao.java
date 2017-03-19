d17b7c88161d65e6321b5524af54459968058d34
nayararibeiro <nayararibeiro@gmail.com>
2017-03-19 18:20:46 -0300

    [~NayaraRibeiro] Add repostas incorretas no JSON.

--- anatomy-REST/src/main/java/br/com/appanatomy/dao/ConnectionDao.java
+++ anatomy-REST/src/main/java/br/com/appanatomy/dao/ConnectionDao.java
@@ -16,12 +16,6 @@ public class ConnectionDao {
 
     private PreparedStatement statement;
 
-    protected SubTheme retrieveSubThemeByName(String name) throws SQLException {
-        return query("SELECT * FROM app_anatomy.sub_theme inner join app_anatomy.question" +
-                " on app_anatomy.question.sub_theme_id = app_anatomy.sub_theme.id inner join app_anatomy.answer" +
-                " on app_anatomy.question.id = app_anatomy.answer.question_id WHERE app_anatomy.sub_theme.name LIKE '"+ name +"'");
-    }
-
     protected void openDBConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.jdbc.Driver");
         connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/app_anatomy?useUnicode=yes&characterEncoding=UTF-8",
@@ -31,6 +25,12 @@ public class ConnectionDao {
     protected void closeDBConnection() throws SQLException {
         statement.close();
         connection.close();
+    }
+
+    protected SubTheme retrieveSubThemeByName(String name) throws SQLException {
+        return query("SELECT * FROM app_anatomy.sub_theme inner join app_anatomy.question" +
+                " on app_anatomy.question.sub_theme_id = app_anatomy.sub_theme.id inner join app_anatomy.answer" +
+                " on app_anatomy.question.id = app_anatomy.answer.question_id WHERE app_anatomy.sub_theme.name LIKE '"+ name +"'");
     }
 
     private SubTheme query(String query) {
@@ -58,10 +58,32 @@ public class ConnectionDao {
         List<Question> questions = new ArrayList<Question>();
         do {
             questions.add(new Question(resultSet.getInt(4), resultSet.getString("title"),
-                    new Answer(resultSet.getInt(8), resultSet.getString("answer"))));
+                    new Answer(resultSet.getInt(8), resultSet.getString("answer")), retrieveIncorrectAnswers(resultSet.getInt(8))));
         } while (resultSet.next());
 
         return questions;
     }
 
+    private List<Answer> retrieveIncorrectAnswers(int id) {
+        List<Answer> incorrectAnswers = queryRandom ("SELECT * FROM app_anatomy.answer WHERE app_anatomy.answer.id != "+ id +" ORDER BY RAND() LIMIT 3");
+        return incorrectAnswers;
+    }
+
+    private List<Answer> queryRandom (String query){
+
+        List<Answer> incorrectAnswers = new ArrayList<Answer>();
+
+        try {
+            statement = connection.prepareStatement(query);
+            resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()){
+                incorrectAnswers.add(new Answer(resultSet.getInt(1), resultSet.getString("answer")));
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Consulta Inválida!");
+        }
+        return incorrectAnswers;
+    }
 }
--- anatomy-REST/src/main/java/br/com/appanatomy/model/Question.java
+++ anatomy-REST/src/main/java/br/com/appanatomy/model/Question.java
@@ -1,14 +1,18 @@
 package br.com.appanatomy.model;
 
+import java.util.List;
+
 public class Question {
     private int id;
     private String title;
     private Answer correctAnswer;
+    private List<Answer> incorrectAnswers;
 
-    public Question(int id, String title, Answer answer) {
+    public Question(int id, String title, Answer answer, List<Answer> incorrectAnswers) {
         this.id = id;
         this.title = title;
         this.correctAnswer = answer;
+        this.incorrectAnswers = incorrectAnswers;
 
     }
 
@@ -17,10 +21,16 @@ public class Question {
     }
 
     public String getTitle() {
+
         return title;
     }
 
     public Answer getCorrectAnswer() {
         return correctAnswer;
     }
+
+    public List<Answer> getIncorrectAnswers() {
+        return incorrectAnswers;
+    }
+
 }
